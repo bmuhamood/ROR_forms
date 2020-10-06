62312a67966a4ff0ac3897f49551a506c34e485f
balerum03 <adafer123.angra@gmail.com>
2020-10-06 14:26:33 -0500

    worked on the user controller and started working using helpers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,23 @@
 class UsersController < ApplicationController
-  
+
   def new
   	@user = User.new
   end
 
   def create
+    @user = User.new(name="User[username]", name ="User[email]", name ="User[password]")
+    #@user = User.new(user_params)
+    @user.save
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
   end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:username,:email,:password)
+  end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+  <%= form_tag("/create", method: "get") do %>
+    <%= username_tag(:q, "username") %>
+    <%= text_field_tag(:q) %>
+    <%= email_tag(:q, "e-mail") %>
+    <%= text_field_tag(:q) %>
+    <%= password_tag(:q) %>
+    <%= text_field_tag(:q) %>
+    <%= submit_tag("Create") %>
+  <%= end %>
 end
