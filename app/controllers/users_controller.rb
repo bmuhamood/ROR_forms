df603a81cc443cfd5f0a203a1734461706a3cc5d
balerum03 <adafer123.angra@gmail.com>
2020-10-07 16:24:10 -0500

    Made changes requested

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,6 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    @user.save
     if @user.save
       redirect_to new_user_path
     else
@@ -19,7 +18,6 @@ class UsersController < ApplicationController
 
   def update
     @user = User.new(user_params)
-    @user.save
     if @user.save
       redirect_to edit_user_path
     else
